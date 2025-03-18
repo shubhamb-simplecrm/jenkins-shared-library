a40433b3c01af787594dd7ae185088ef0b266208
shubhamb-simplecrm <shubham.bawane@simple.works>
2025-03-18 17:01:28 +0530

    Update secureEnv.groovy

--- vars/secureEnv.groovy
+++ vars/secureEnv.groovy
@@ -1,1 +1,17 @@
+// vars/secureEnv.groovy
+def call() {
+    return [
+        JENKINS_JOB_NAME             : "v267",
+        GIT_HUB_ACCOUNT_OWNER        : 'riteshd2',
+        GIT_HUB_REPO_OWNER           : 'simplecrm-projects',
+        GIT_HUB_REPO_NAME            : 'simplecrm_v267',
+        GIT_HUB_URL                  : "https://github.com/simplecrm-projects/simplecrm_v267.git",
+        TARGET_BRANCH                : "dev",
+        JENKINS_GLOBAL_CREDENTIAL_TOKEN : 'Implementataion-ID-Token1',
+        REMOTE_SERVER_CREDENTIAL     : 'Remote-Server-Demo',
+        REMOTE_SERVER                : 'shubhamb@172.22.7.71',
+        REMOTE_PORT                  : "58689",
+        REMOTE_PATH                  : '/application/product/code/v267/'
+    ]
+}
 
